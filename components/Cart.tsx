582dec0151e8c68ed05f0fb0848e75824a5616d1
Jeff Lam <lmh.jeff@gmail.com>
2022-11-28 17:34:44 +0800

    shopping cart basically completed

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import { CubeTransparentIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+
 const Cart = () => {
+  const [items, setItems] = useState<boolean>(false);
   return (
-    <div className="flex flex-col w-[20%] bg-gray-400 h-screen">
-      I am Shopping Cart
+    <div className="flex flex-col w-[20%] bg-gray-400 h-screen justify-center items-center">
+      {!items ? (
+        <div className="flex flex-col items-center">
+          <CubeTransparentIcon className="h-8 w-8 text-gray-600" />
+          <div className="text-gray-600">NO ITEM ADDED!!</div>
+        </div>
+      ) : (
+        <div>Many Items</div>
+      )}
     </div>
   );
 };

d73db86ed8d1dc7249c0388096e61d9a599b52ef
Jeff Lam <lmh.jeff@gmail.com>
2022-12-30 00:58:32 +0800

    drag and drop testing

--- app/(user)/orders/page.tsx
+++ app/(user)/orders/page.tsx
@@ -1,16 +1,105 @@
+"use client";
+import { DragDropContext, Draggable, Droppable } from "@hello-pangea/dnd";
+import { useCallback, useState } from "react";
+
+const marvelList = [
+  {
+    id: "ironMan",
+    name: "Iron Man",
+    color: "#f43a72",
+  },
+  {
+    id: "captain",
+    name: "Captain",
+    color: "#2340ff",
+  },
+  {
+    id: "hulk",
+    name: "Hulk",
+    color: "#5cfc94",
+  },
+  {
+    id: "thanos",
+    name: "Thanos",
+    color: "#fff763",
+  },
+];
+
 const Orders = () => {
+  const [character, setCharacter] = useState(marvelList);
+
+  // // using useCallback is optional
+  // const onBeforeCapture = useCallback(() => {
+  //   /*...*/
+  // }, []);
+  // const onBeforeDragStart = useCallback(() => {
+  //   /*...*/
+  // }, []);
+  // const onDragStart = useCallback(() => {
+  //   /*...*/
+  // }, []);
+  // const onDragUpdate = useCallback(() => {
+  //   /*...*/
+  // }, []);
+  const onDragEnd = (result: any) => {
+    // the only one that is required
+    console.log(result);
+    if (!result.destination) return;
+    const items = Array.from(character);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+
+    items.splice(result.destination.index, 0, reorderedItem);
+    console.log("items", items);
+    setCharacter(items);
+  };
+
+  const remove = (id: string) => {
+    setCharacter(character.filter((cha) => cha.id !== id));
+  };
+
   return (
-    <div className="text-white">
+    <div className="text-white flex flex-col justify-center items-center mx-auto">
       <p>I am orders page</p>
-      <select
-        className="w-1/4 bg-transparent"
-        // onChange={(e) => handleDiscount(e.target.value)}
-        defaultValue="sellingPrice"
-      >
-        <option disabled>Discount</option>
-        <option value="sellingPrice">減價</option>
-        <option value="percentage">折扣 %</option>
-      </select>
+      <div className="w-1/2">
+        <DragDropContext onDragEnd={onDragEnd}>
+          <Droppable droppableId="character">
+            {(provided) => (
+              <ul
+                className="flex flex-col space-y-4"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+              >
+                {character.map(({ id, name, color }, index) => {
+                  return (
+                    <Draggable key={id} draggableId={id} index={index}>
+                      {(provided) => (
+                        <li
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          className="flex flex-row items-center w-full p-2 border-white border-2 rounded-lg list-none"
+                        >
+                          <div
+                            className={`w-8 h-8 rounded-full bg-[${color}]`}
+                          ></div>
+                          <p>{name}</p>
+                          <button
+                            onClick={() => remove(id)}
+                            className="w-4 h-4 bg-red-400 rounded-full"
+                          >
+                            x
+                          </button>
+                        </li>
+                      )}
+                    </Draggable>
+                  );
+                })}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
     </div>
   );
 };

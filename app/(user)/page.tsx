be803e4630d14bd48cca7d59fc145066f7a02fa5
Jeff Lam <lmh.jeff@gmail.com>
2023-01-23 02:45:24 +0800

    created doughnnut chart component

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,19 +1,42 @@
+//@ts-nocheck
 import { groq } from "next-sanity";
 import { previewData } from "next/headers";
+import { Doughnut } from "react-chartjs-2";
+import DoughnutChart from "../../components/DoughnutChart";
 import PreviewSuspense from "../../components/PreviewSuspense";
 import { client } from "../../lib/sanity.client";
-
-const todayQuery = groq`
-  math::sum(*[_type == "orders" && _createdAt >= "2023-01-10T16:00:00.000Z"][].total)
-`;
 
 const totalQuery = groq`
   math::sum(*[_type == "orders"][].total)
 `;
 
+const cashQuery = groq`
+  math::sum(*[_type == "orders" && paymentMethod == "CASH"][].total)
+`;
+
+const paymeQuery = groq`
+  math::sum(*[_type == "orders" && paymentMethod == "PAYME"][].total)
+`;
+
+const fpsQuery = groq`
+  math::sum(*[_type == "orders" && paymentMethod == "FPS"][].total)
+`;
+
+const octopusQuery = groq`
+  math::sum(*[_type == "orders" && paymentMethod == "OCTOPUS"][].total)
+`;
+
+const tapandGoQuery = groq`
+  math::sum(*[_type == "orders" && paymentMethod == "TAPNGO"][].total)
+`;
+
 const Home = async () => {
-  const today = await client.fetch(todayQuery);
-  const all = await client.fetch(totalQuery);
+  const total = await client.fetch(totalQuery);
+  const cash = await client.fetch(cashQuery);
+  const payme = await client.fetch(paymeQuery);
+  const fps = await client.fetch(fpsQuery);
+  const octopus = await client.fetch(octopusQuery);
+  const tapngo = await client.fetch(tapandGoQuery);
 
   // if (previewData()) {
   //   return (
@@ -34,10 +57,26 @@ const Home = async () => {
   return (
     <div className="text-white">
       <div>
-          <h1>All:</h1>
-        <h1>{all}</h1>
-        <h1>Today:</h1>
-        <h1>{today}</h1>
+        <h1>All:</h1>
+        <h1>{total}</h1>
+        <div>{cash}</div>
+        {/* <DoughnutChart
+        // total={123}
+        // cash={2}
+        // payme={2}
+        // fps={2}
+        // octopus={2}
+        // tapngo={2}
+        /> */}
+
+        <DoughnutChart
+          total={total}
+          cash={cash}
+          payme={payme}
+          fps={fps}
+          octopus={octopus}
+          tapngo={tapngo}
+        />
       </div>
     </div>
   );
---
+++ components/DoughnutChart.tsx
@@ -0,0 +1,52 @@
+//@ts-nocheck
+"use client";
+
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const DoughnutChart = ({ cash, payme, fps, octopus, tapngo }) => {
+  const data = {
+    labels: ["Cash", "Payme", "FPS", "Octopus", "TapnGo"],
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [cash, payme, fps, octopus, tapngo],
+        backgroundColor: [
+          "#65b5ff",
+          "#FF2828",
+          "#97ff87",
+          "#ffa982",
+          "#4f7f8a",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
+
+  // const data = {
+  //   labels: ["Red", "Blue", "Yellow"],
+  //   datasets: [
+  //     {
+  //       label: "My First Dataset",
+  //       data: [300, 50, 100],
+  //       backgroundColor: [
+  //         "rgb(255, 99, 132)",
+  //         "rgb(54, 162, 235)",
+  //         "rgb(255, 205, 86)",
+  //       ],
+  //       hoverOffset: 4,
+  //     },
+  //   ],
+  // };
+
+  const options = {};
+
+  return (
+    <div>
+      <Doughnut data={data} />
+    </div>
+  );
+};
+
+export default DoughnutChart;
